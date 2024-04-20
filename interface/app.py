f2e0c594dc1d6f50e76844df829ddef3f784daaf
stepanMY <o.malih2012@gmail.com>
2024-04-20 18:14:09 +0300

    new interface

--- interface/app.py
+++ interface/app.py
@@ -1,28 +1,15 @@
-import random
-
-import requests
-from flask import Flask, render_template
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
-
-@app.route("/health")
-def health():
-    return "healthy"
-
-
-@app.route("/")
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('app_frontend.html', prediction_text="")
 
+@app.route('/predict', methods=['GET','POST'])
+def predict():
+    a_description = request.form.get('description')
+    return render_template('app_frontend.html', prediction_text=a_description)
 
-@app.route("/get_quote")
-def quote():
-    quote = requests.get("http://controller:5000/quote").text
-    print("quote - ", quote)
-
-    return render_template("quote.html", quote=quote)
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=5001)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True, port=5001)
