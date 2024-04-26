a11bb31dce96d1e36600613fde7ec82e79e1897d
stepanMY <o.malih2012@gmail.com>
2024-04-27 00:19:57 +0300

    update with 3 containers

--- interface/app.py
+++ interface/app.py
@@ -1,15 +1,17 @@
+import requests
 from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('app_frontend.html', prediction_text="")
+    return render_template('app_frontend.html', prediction_text='')
 
 @app.route('/predict', methods=['GET','POST'])
 def predict():
     a_description = request.form.get('description')
-    return render_template('app_frontend.html', prediction_text=a_description)
+    quote = requests.get('http://controller:5001/get_quote').text
+    return render_template('app_frontend.html', prediction_text=quote)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=5001)
+    app.run(host='0.0.0.0', debug=True, port=5000)
